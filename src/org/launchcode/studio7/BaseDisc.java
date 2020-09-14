8e1af29d1fb43c43b11fa312bc59580c8ed55cdf
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-14 15:11:01 -0500

    Untrack files in .gitignore

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,14 @@
+package org.launchcode.studio7;
+
+import java.util.HashMap;
+
+public class BaseDisc {
+
+    private String name;
+    private double storage;
+    private double storageUsed;
+    private String discType;
+    private HashMap<String, String> contents;
+
+
+}
